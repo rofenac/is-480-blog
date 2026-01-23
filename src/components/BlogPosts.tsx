e11e77a684a2e8fe3d055503b9578e2032bcb571
rofenac <daviderr3@gmail.com>
2026-01-23 15:53:24 -0800

    Week 3 Post Correction

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -29,7 +29,7 @@ const posts: BlogPost[] = [
   {
     id: 2,
     title: 'Time Management',
-    excerpt: 'Choosing an internship over a project to work on installing NDG Netlab+ with my supervisors. Navigating the VMWare to Proxmox transition and planning the network infrastructure.',
+    excerpt: 'Not a whole lot happened this week.',
     body: `Not a whole happened this week. I kind of dropped the ball on my internship duties because I was swamped with school work. Next week I will try to get back on track. I have three concrete goals for the coming week. First, install the Netlab+ network switches. Second, obtain the login credentials for the Netlab+ control servers. Third, convince Prof. Blackwell to go ahead with Proxmox 8 instead of waiting for Proxmox 9 support. If I can those all done, I'll feel good about my internship progress.`,
     date: 'Jan 24, 2026',
     tags: ['Netlab+', 'Proxmox', 'Infrastructure'],
