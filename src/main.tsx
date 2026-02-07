69f08458f133d493f447dfe0a30b1409e8ac252e
rofenac <daviderr3@gmail.com>
2026-02-06 16:25:40 -0800

    Frontend Code Correction

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(useGSAP);
